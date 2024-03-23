d0f6d8b462b7d5076f9b245474c1870f91d11437
travel-needs <164065823+travel-needs@users.noreply.github.com>
2024-03-23 19:10:26 +0530

    feat: slack key update

--- server/index.js
+++ server/index.js
@@ -82,7 +82,7 @@ app.post('/register', (req, res) => {
 
 async function slackNotification(message, date) {
     console.log('sending slack notification....')
-    const url = 'https://hooks.slack.com/services/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'
+    const url = 'https://hooks.slack.com/services/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'
     const data = {
         text: `New registration: \nName: ${message.name} \nNumber: ${message.number} \nDate: ${date}`
     }
